c4c7893b8a413339baf63f9f167cbf689ae87a62
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-11-12 19:05:20 -0500

    mvp and finished one stretch goal

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
-import React, { useState } from 'react';
-
+import React, {useState} from 'react';
 import SavedList from './Movies/SavedList';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+	const [savedList, setSavedList] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+	const addToSavedList = movie => {
+		setSavedList( [...savedList, movie] );
+	};
 
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+	return (
+		<Router>
+			<div>
+				<SavedList list={savedList}/>
+
+				<Route exact path='/' component={MovieList}/>
+				<Route path='/movie/:id' render={props => {
+					return <Movie {...props}/>
+				}}/>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
